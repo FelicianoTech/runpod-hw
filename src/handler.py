ffb65216b7cf5923fe60a434ff117704a65f90dc
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2024-12-19 13:25:45 -0500

    Normalize image dimensions

--- src/handler.py
+++ src/handler.py
@@ -1,4 +1,4 @@
-""" Example handler file. """
+""" SDXL-Turbo backend. """
 
 import runpod
 from diffusers import AutoPipelineForText2Image
@@ -7,24 +7,41 @@ import base64
 import io
 import time
 
-# If your handler runs inference on a model, load the model here.
-# You will want models to be loaded into memory before starting serverless.
-
 try:
-    pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
+    pipe = AutoPipelineForText2Image.from_pretrained(
+        "stabilityai/sdxl-turbo",
+        torch_dtype=torch.float16,
+        variant="fp16",
+    )
     pipe.to("cuda")
 except RuntimeError:
     quit()
 
 def handler(job):
+
     """ Handler function that will be used to process jobs. """
     job_input = job['input']
     prompt = job_input['prompt']
-    width = job_input['width']
-    height = job_input['height']
+    imageWidth = job_input['width']
+    imageHeight = job_input['height']
+
+    # Normalize the image dimensions. They have to be divisible by 8.
+    widthRemainder = imageWidth % 8
+    heightRemainder = imageHeight % 8
+    if widthRemainder != 0:
+        imageWidth += 8 - widthRemainder
+    if heightRemainder != 0:
+        imageHeight += 8 - heightRemainder
 
     time_start = time.time()
-    image = pipe(prompt=prompt, width=width, height=height, num_inference_steps=1, guidance_scale=0.0).images[0]
+    image = pipe(
+        prompt=prompt,
+        width=imageWidth,
+        height=imageHeight,
+        num_inference_steps=5,
+        guidance_scale=0.0,
+    ).images[0]
+
     print(f"Time taken: {time.time() - time_start}")
 
     buffer = io.BytesIO()
